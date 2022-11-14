bf02f1b439d988a4f7078a13d7321e5220ce305e
Said Saliev <repsaid@gmail.com>
2022-11-14 14:36:02 +0600

    added a detailed product view page

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -5,8 +5,11 @@ from webapp.models import Products
 
 def index_view(request):
     products = Products.objects.order_by('name', 'category')
-    context = {
-        'products': products,
-    }
-
+    context = {'products': products}
     return render(request, 'index.html', context)
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Products, pk=pk)
+    context = {'product': product}
+    return render(request, 'view.html', context)
