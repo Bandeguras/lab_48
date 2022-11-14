a011b3d2dc75cef4aeeaa0bd1efc1e41f245a3cf
Said Saliev <repsaid@gmail.com>
2022-11-14 14:21:46 +0600

    add main page

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Products
 # Create your views here.
+
+
+def index_view(request):
+    products = Products.objects.order_by('name', 'category')
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'index.html', context)
