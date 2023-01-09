03fabcbc5563db2d30b5884eb7c048da6a34c95d
Said Saliev <repsaid@gmail.com>
2023-01-09 16:04:06 +0600

    добавил проверку прав доступа пользователя back/front

--- source/webapp/views/products_views.py
+++ source/webapp/views/products_views.py
@@ -1,8 +1,7 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.utils.http import urlencode
-
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from webapp.models import Products
 from webapp.form import ProductForm, Search
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -52,23 +51,22 @@ class ProductDetail(DetailView):
         return Products.objects.all().filter(remains__gt=0)
 
 
-class ProductCreate(CreateView):
+class ProductCreate(PermissionRequiredMixin, CreateView):
     template_name = 'product/create.html'
     model = Products
     context_object_name = 'product'
     form_class = ProductForm
-
+    permission_required = 'webapp.add_products'
     def get_success_url(self):
         return reverse('product_view', kwargs={'pk': self.object.pk})
 
 
-
-
-class ProductUpdate(UpdateView):
+class ProductUpdate(PermissionRequiredMixin, UpdateView):
     template_name = 'product/update.html'
     model = Products
     context_object_name = 'product'
     form_class = ProductForm
+    permission_required = 'webapp.change_products'
 
     def get_queryset(self):
         return Products.objects.all().filter(remains__gt=0)
@@ -77,12 +75,11 @@ class ProductUpdate(UpdateView):
         return reverse('product_view', kwargs={'pk': self.object.pk})
 
 
-
-
-class ProductDelete(DeleteView):
+class ProductDelete(PermissionRequiredMixin, DeleteView):
     template_name = 'product/delete.html'
     model = Products
     context_object_name = 'product'
+    permission_required = 'webapp.delete_products'
 
     def get_queryset(self):
         return Products.objects.all().filter(remains__gt=0)
